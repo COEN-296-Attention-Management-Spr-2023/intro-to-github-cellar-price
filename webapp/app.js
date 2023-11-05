c86c05e6a4cc9e508e7db3e4f0e4d6a13c59f68c
chibiininja <inouyedarren@gmail.com>
2023-11-05 14:16:53 -0800

    Got website search working

--- webapp/app.js
+++ webapp/app.js
@@ -1,6 +1,6 @@
 // Setup MiniSearch
 const ItemSearch = new MiniSearch({
-  fields: ['product', 'price'],
+  fields: ['product'],
   storeFields: ['product', 'price']
 })
 
@@ -10,6 +10,7 @@ const $search = document.querySelector('.Search')
 const $searchInput = document.querySelector('.Search input')
 const $clearButton = document.querySelector('.Search button.clear')
 const $productList = document.querySelector('.ProductList')
+const $suggestionList = document.querySelector('.SuggestionList')
 const $options = document.querySelector('.AdvancedOptions form')
 
 // Fetch and index data
@@ -37,6 +38,8 @@ $searchInput.addEventListener('input', (event) => {
   const results = (query.length > 1) ? getSearchResults(query) : []
   renderSearchResults(results)
 
+  const suggestions = (query.length > 1) ? getSuggestions(query) : []
+  renderSuggestions(suggestions)
 })
 
 // Clicking on clear button clears search and suggestions
@@ -45,6 +48,46 @@ $clearButton.addEventListener('click', () => {
   $searchInput.focus()
 
   renderSearchResults([])
+  renderSuggestions([])
+})
+
+// Clicking on a suggestion selects it
+$suggestionList.addEventListener('click', (event) => {
+  const $suggestion = event.target
+
+  if ($suggestion.classList.contains('Suggestion')) {
+    const query = $suggestion.innerText.trim()
+    $searchInput.value = query
+    $searchInput.focus()
+
+    const results = getSearchResults(query)
+    renderSearchResults(results)
+    renderSuggestions([])
+  }
+})
+
+// Pressing up/down/enter key while on search bar navigates through suggestions
+$search.addEventListener('keydown', (event) => {
+  const key = event.key
+
+  if (key === 'ArrowDown') {
+    selectSuggestion(+1)
+  } else if (key === 'ArrowUp') {
+    selectSuggestion(-1)
+  } else if (key === 'Enter' || key === 'Escape') {
+    $searchInput.blur()
+    renderSuggestions([])
+  } else {
+    return
+  }
+  const query = $searchInput.value
+  const results = getSearchResults(query)
+  renderSearchResults(results)
+})
+
+// Clicking outside of search bar clears suggestions
+$app.addEventListener('click', (event) => {
+  renderSuggestions([])
 })
 
 // Changing any advanced option triggers a new search with the updated options
@@ -65,7 +108,13 @@ const searchOptions = {
 
 const getSearchResults = (query) => {
   const searchOptions = getSearchOptions()
-  return miniSearch.search(query, searchOptions).map(({ id }) => productsById[id])
+  return ItemSearch.search(query, searchOptions).map(({ id }) => productsById[id])
+}
+
+const getSuggestions = (query) => {
+  return ItemSearch.autoSuggest(query, { boost: { product: 5 } })
+    .filter(({ suggestion, score }, _, [first]) => score > first.score / 4)
+    .slice(0, 5)
 }
 
 const renderSearchResults = (results) => {
@@ -85,13 +134,38 @@ const renderSearchResults = (results) => {
   }
 }
 
+const renderSuggestions = (suggestions) => {
+  $suggestionList.innerHTML = suggestions.map(({ suggestion }) => {
+    return `<li class="Suggestion">${suggestion}</li>`
+  }).join('\n')
+
+  if (suggestions.length > 0) {
+    $app.classList.add('hasSuggestions')
+  } else {
+    $app.classList.remove('hasSuggestions')
+  }
+}
+
+const selectSuggestion = (direction) => {
+  const $suggestions = document.querySelectorAll('.Suggestion')
+  const $selected = document.querySelector('.Suggestion.selected')
+  const index = Array.from($suggestions).indexOf($selected)
+
+  if (index > -1) {
+    $suggestions[index].classList.remove('selected')
+  }
+
+  const nextIndex = Math.max(Math.min(index + direction, $suggestions.length - 1), 0)
+  $suggestions[nextIndex].classList.add('selected')
+  $searchInput.value = $suggestions[nextIndex].innerText
+}
+
 const getSearchOptions = () => {
   const formData = new FormData($options)
   const searchOptions = {}
 
   searchOptions.fuzzy = formData.has('fuzzy') ? 0.2 : false
   searchOptions.prefix = formData.has('prefix')
-  searchOptions.fields = formData.getAll('fields')
   searchOptions.combineWith = formData.get('combineWith')
 
   return searchOptions
