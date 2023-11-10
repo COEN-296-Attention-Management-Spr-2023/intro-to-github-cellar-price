df1285a2d09f1c684201d233873bfd8a8fca4389
roblee <roblee04@gmail.com>
2023-11-09 23:30:29 -0800

    changed NAME to PRODUCT in csv and json

--- db/insert_into_json.py
+++ db/insert_into_json.py
@@ -75,7 +75,7 @@ def main():
             unique_names = set()
             # write to csv and convert that to json
             csv_file = 'item_info.csv'
-            header = ["id", "name", "price", "img", "category"]
+            header = ["id", "product", "price", "img", "category"]
             if not os.path.exists(csv_file):
                 f_output = open(csv_file ,'w')
                 writer = csv.DictWriter(f_output, fieldnames=header)
@@ -99,7 +99,7 @@ def main():
                 item = mega_list[elem]
                 if item.name.replace('-', ' ') not in unique_names:
                     writer.writerow(
-                        {'id': id_count, 'name': item.name.replace('-', ' '), 'price': '%.2f' % item.price, 
+                        {'id': id_count, 'product': item.name.replace('-', ' '), 'price': '%.2f' % item.price, 
                          'img': "IMG", 'category':[]}
                         )
                     id_count += 1
